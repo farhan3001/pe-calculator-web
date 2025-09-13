ad4049472f7fe87a1dc390606b38331a979bfd9c
muhammad.simatupang <muhammad.simatupang@artajasa.co.id>
2025-09-14 00:51:53 +0700

    udpate API implementation

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
-const API_URL = 'https://your-backend-api.com/submit'
+const BASE_URL = import.meta.env.BASE_URL
+
+const URL_POST_PE = import.meta.env.URL_POST_PE
+const API_KEY = import.meta.env.API_KEY
 
 export const submitFormData = (data) => {
-  return axios.post(API_URL, data)
+  return axios.post(`${BASE_URL}${URL_POST_PE}`, data, {
+    headers: {
+      'x-api-key': API_KEY,
+      'Content-Type': 'application/json',
+    }
+  })
 }
